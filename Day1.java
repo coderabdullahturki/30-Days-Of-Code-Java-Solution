61fbdd748d96570eca095a81c6e2bfce57d28765
Abdullah Turky <abdullahturki087@gmail.com>
2021-04-30 02:17:54 +0600

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -21,11 +21,6 @@ public class Day1 {
 
 
 
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back              and review the Tutorial closely.
-
-        /* Print the sum of both integer variables on a new line. */
-
         System.out.println(i + a);
 
         /* Print the sum of the double variables on a new line. */
